0231274a366193fad24efa76d80b3fd3702945e4
anxy <jssaxy@163.com>
2016-11-17 23:04:05 +0800

    添加HelloWorld控件

--- app/src/main/java/com/example/gittestproject/MainActivity.java
+++ app/src/main/java/com/example/gittestproject/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.gittestproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHelloWorld = (TextView)findViewById(R.id.hello_world);
+
+        tvHelloWorld.setText("你好，世界！");
     }
 }
